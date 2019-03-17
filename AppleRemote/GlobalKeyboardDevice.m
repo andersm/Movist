e5e59f75370a28e03bda29c1ba7bbee99786609d
Anders Montonen <Anders.Montonen@iki.fi>
2019-03-17 21:41:51 +0200

    Move static function declaration from header to implementation file

--- AppleRemote/GlobalKeyboardDevice.m
+++ AppleRemote/GlobalKeyboardDevice.m
@@ -55,6 +55,7 @@
  mac.remotecontrols.GlobalKeyboardDevice.playhold_keycode
  */
 
+static OSStatus hotKeyEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEvent, void* refCon );
 
 @implementation GlobalKeyboardDevice
 
